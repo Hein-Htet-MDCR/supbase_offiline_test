8c90ec7c3128d5bee693e9700e2e97a09dccfd70
Hein-Htet-MDCR <heinhtet@myanmardcr.com>
2026-09-25 11:10:27 +0630

    Update Supabase URL and anon key in configuration

--- lib/core/remote/supabase_config.dart
+++ lib/core/remote/supabase_config.dart
@@ -1,9 +1,9 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String supabaseUrl = 'https://mipcriujwmknznpdbhti.supabase.co';
+  static const String supabaseUrl = 'https://.supabase.co';
   static const String supabaseAnonKey =
-      'sb_publishable_lNbflvLmR4oQGX5jnlBYCA_ZawxYxaF';
+      'youanonokey';
 
   static Future<void> initialize() async {
     await Supabase.initialize(
